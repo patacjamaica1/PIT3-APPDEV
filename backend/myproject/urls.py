b1cc950592c5e529c6b2c4ef017d987bb1d62134
JAM <126861194+patacjamaica1@users.noreply.github.com>
2025-04-04 10:47:21 +0800

    Initial Commit

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp.views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),  # Updated: Make sure the API prefix is set correctly
+    path('api/', include(router.urls)),
 ]
 
