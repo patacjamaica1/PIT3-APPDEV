2477e6475d9964f2d617d25000adc9bb774c6f15
JAM <126861194+patacjamaica1@users.noreply.github.com>
2025-04-11 09:29:25 +0800

    added in settings

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
